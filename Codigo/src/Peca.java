961203f575c065cc183c0d63dd027149a9af32e2
danielyoshizawa <yoshidanielcwb@gmail.com>
2016-11-20 13:09:32 -0200

    Criando os metodos de Peca

--- Codigo/src/Peca.java
+++ Codigo/src/Peca.java
@@ -1,5 +1,15 @@
-/**
- * Created by daniel on 11/20/16.
- */
+
 public class Peca {
+
+    protected String nome;
+    protected Grade gradePeca;
+    protected String cor;
+
+    public Peca() {
+
+    }
+
+    public boolean ehPosicaoValida(float posicaoX, float posicaoY) {
+        return false;
+    }
 }
