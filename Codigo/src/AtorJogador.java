602b9349d0ff93af0aeed5352c7cedf1cdfdd48b
danielyoshizawa <yoshidanielcwb@gmail.com>
2016-11-20 12:44:08 -0200

    Criando os metodos da classe AtorJogador

--- Codigo/src/AtorJogador.java
+++ Codigo/src/AtorJogador.java
@@ -1,5 +1,61 @@
-/**
- * Created by daniel on 11/20/16.
- */
+import java.util.ArrayList;
+
 public class AtorJogador {
+
+    protected String idUsuario;
+    // TODO : Aqui será criada uma dependencia ciclica, rever isso
+    protected AtorNetGames rede;
+    protected Jogo jogo;
+    // TODO : Criar a view
+    // protected View view;
+
+    public AtorJogador() {
+
+    }
+
+    public boolean conectar() {
+        return false;
+    }
+
+    public String obterDadosConexao() {
+        return "";
+    }
+
+    public int iniciarPartida() {
+        return 0;
+    }
+
+    public boolean avaliarInterrupcao() {
+        return false;
+    }
+
+    public int desconectar() {
+        return 0;
+    }
+
+    public void tratarInciarPartida() {
+
+    }
+
+    public void receberJogada(Jogada jogada) {
+
+    }
+
+    public int tratarClick(float posicaoX, float posicaoY) {
+        return 0;
+    }
+
+    public void enviarJogada(Peca peca, int posicaoX, int posicaoY, String idUsuario) {
+
+    }
+
+    // TODO : Analizar como este metodo sera implementado
+    public ArrayList<Integer> informarEstado() {
+
+        return null;
+    }
+
+    public ArrayList<Integer> notificarPecasDisponiveis() {
+        return null;
+    }
 }
