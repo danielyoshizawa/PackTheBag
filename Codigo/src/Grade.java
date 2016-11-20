ffe6dc8b22e7609e82c3fe6704541e49301cb808
danielyoshizawa <yoshidanielcwb@gmail.com>
2016-11-20 12:49:01 -0200

    Criando metodos da Grade

--- Codigo/src/Grade.java
+++ Codigo/src/Grade.java
@@ -1,5 +1,34 @@
-/**
- * Created by daniel on 11/20/16.
- */
+import java.util.ArrayList;
+
 public class Grade {
+
+    // TODO : criar uma maneira melhor de armazenar as posicoes
+    protected boolean [][] posicoes;
+    protected int tamanhoX;
+    protected int tamanhoY;
+
+    public Grade() {
+
+    }
+
+    public void iniciar(int numLinhas, int numColunas) {
+
+    }
+
+    // TODO : criar uma entidade posicao ou algo assim, pra representar essas tuplas
+    public void configurarPosicoesOcupadas(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+
+    }
+
+    public void esvaziar() {
+
+    }
+
+    public void excluirGrade() {
+
+    }
+
+    public boolean encaixa(Peca peca) {
+        return false;
+    }
 }
