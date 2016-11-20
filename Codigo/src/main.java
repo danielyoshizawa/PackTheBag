aef3b08d9fa09514daf54aae481a0db4b73e3807
danielyoshizawa <yoshidanielcwb@gmail.com>
2016-11-20 18:18:58 -0200

    Removing Slick2d from the project

--- Codigo/src/main.java
+++ Codigo/src/main.java
@@ -1,21 +1,7 @@
-import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.SlickException;
-
 
 public class main {
 
     public static void main(String [] args) {
-        ConectarViewState conectarViewState = new ConectarViewState();
-        GameState gameState = new GameState("Pack the bag");
-        gameState.addState(conectarViewState);
-
-        try {
-            AppGameContainer gameContainer = new AppGameContainer(gameState, 1200, 800, false);
-            gameContainer.start();
-        } catch (SlickException e) {
-            e.printStackTrace();
-        }
-
 
     }
 }
