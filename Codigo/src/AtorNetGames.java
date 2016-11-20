33a8f5d9e639baf3f45a8d603a1b3bd7d85347f4
danielyoshizawa <yoshidanielcwb@gmail.com>
2016-11-20 12:33:43 -0200

    Criando metodos de AtorNetGames

--- Codigo/src/AtorNetGames.java
+++ Codigo/src/AtorNetGames.java
@@ -1,5 +1,64 @@
-/**
- * Created by daniel on 11/20/16.
- */
-public class AtorNetGames {
+import br.ufsc.inf.leobr.cliente.OuvidorProxy;
+import br.ufsc.inf.leobr.cliente.Proxy;
+
+public class AtorNetGames implements OuvidorProxy {
+
+    protected Proxy proxy;
+    protected AtorJogador atorJogador;
+
+    public AtorNetGames(AtorJogador atorJogador) {
+        super();
+        this.atorJogador = atorJogador;
+        proxy = Proxy.getInstance();
+    }
+
+    public boolean conectar(String servidor, String nome) {
+        return false;
+    }
+
+    public void iniciarPartida() {
+
+    }
+
+    public boolean desconectar() {
+        return false;
+    }
+
+    public String informarNomeAdversario(String idUsuario) {
+        return "";
+    }
+
+    public void enviarJogada(Jogada jogada) {
+
+    }
+
+    @Override
+    public void iniciarNovaPartida(Integer posicao) {
+
+    }
+
+    @Override
+    public void finalizarPartidaComErro(String message) {
+
+    }
+
+    @Override
+    public void receberMensagem(String msg) {
+
+    }
+
+    @Override
+    public void receberJogada(br.ufsc.inf.leobr.cliente.Jogada jogada) {
+
+    }
+
+    @Override
+    public void tratarConexaoPerdida() {
+
+    }
+
+    @Override
+    public void tratarPartidaNaoIniciada(String message) {
+
+    }
 }
