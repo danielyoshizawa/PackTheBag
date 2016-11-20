aafe369d973a1ab13284e9170e16aef31f239bf9
danielyoshizawa <yoshidanielcwb@gmail.com>
2016-11-20 12:54:04 -0200

    Criando os metodos de Jogada

--- Codigo/src/Jogada.java
+++ Codigo/src/Jogada.java
@@ -1,5 +1,16 @@
-/**
- * Created by daniel on 11/20/16.
- */
+
 public class Jogada {
+
+    protected Peca peca;
+    protected Grade posicaoDesejada;
+    protected long tempoDisponivel;
+    protected String idUsuario;
+
+    public Jogada() {
+
+    }
+
+    public void iniciar(Peca peca, int posicaoX, int posicaoY, String idUsuario) {
+        
+    }
 }
