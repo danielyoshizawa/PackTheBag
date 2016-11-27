8d956d34f7c320a6cdf047b441f779c5c738c59a
danielyoshizawa <yoshidanielcwb@gmail.com>
2016-11-27 15:05:09 -0200

    TratarIniciarPartida algoritmo

--- Codigo/src/main.java
+++ Codigo/src/main.java
@@ -1,7 +1,23 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
-public class main {
+public class main extends Application {
 
     public static void main(String [] args) {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Circle circ = new Circle(40,40,30);
+        Group root = new Group(circ);
+        Scene scene = new Scene(root, 1200, 900);
+
+        primaryStage.setTitle("My JavaFX Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
